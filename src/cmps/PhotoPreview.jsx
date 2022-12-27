dcbee77e89545262ddd9272d804fc681fc9b8217
Stav Kotlovsky <stavkot1999@gmail.com>
2022-12-27 15:55:23 +0200

    home page

--- src/cmps/PhotoPreview.jsx
+++ src/cmps/PhotoPreview.jsx
@@ -1,7 +1,6 @@
 import { Link } from 'react-router-dom'
 
 export function PhotoPreview({ imgUrl, photoId }) {
-  console.log(photoId)
   return (
     <Link to={`/photo/${photoId}`} className="photo-preview">
       <img className="photo-preview" src={imgUrl} alt="" />
